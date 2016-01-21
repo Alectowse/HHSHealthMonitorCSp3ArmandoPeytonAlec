9859fb233bd03aeebc25afffcc890f3e23ac5c24
Alectowse <alec.towse@students.sacs.k12.in.us>
2016-01-21 09:53:33 -0500

    JSON implemented

--- app/src/main/java/com/example/at1006749/medapp10/JSONstorer.java
+++ app/src/main/java/com/example/at1006749/medapp10/JSONstorer.java
@@ -1,7 +1,26 @@
 package com.example.at1006749.medapp10;
 
+import android.content.Context;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
 /**
  * Created by 1006749 on 1/21/2016.
  */
-public class JSONstorer {
+public abstract class JSONstorer {
+    protected Context mContext;
+    protected String mFilename;
+    private static final int MainActivity = 0;
+
+    public JSONstorer() {}
+
+    public JSONstorer(Context c, String f) {
+        mContext = c;
+        mFilename = f;
+    }
+
+    public abstract MainActivity load() throws IOException, JSONException;
+    public abstract void save(MainActivity mainactivity) throws JSONException, IOException;
 }
