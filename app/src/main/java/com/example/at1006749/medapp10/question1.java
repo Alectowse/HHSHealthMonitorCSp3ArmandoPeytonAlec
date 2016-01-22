014778e3187b71854bc8ad94cbdb25abe347972b
Alectowse <alec.towse@students.sacs.k12.in.us>
2016-01-22 10:11:40 -0500

    imports and results xml

--- app/src/main/java/com/example/at1006749/medapp10/question1.java
+++ app/src/main/java/com/example/at1006749/medapp10/question1.java
@@ -1,6 +1,9 @@
 package com.example.at1006749.medapp10;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by 1006749 on 1/21/2016.
--- app/src/main/java/com/example/at1006749/medapp10/results.java
+++ app/src/main/java/com/example/at1006749/medapp10/results.java
@@ -1,7 +1,17 @@
 package com.example.at1006749.medapp10;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
 /**
  * Created by 1006749 on 1/21/2016.
  */
 public class results {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.results);
+    }
 }
