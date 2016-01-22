11833211cbb321b2c77871cd0cc91b607e024640
Alectowse <alec.towse@students.sacs.k12.in.us>
2016-01-22 09:50:39 -0500

    The real broken xml intiaition

--- app/src/main/java/com/example/at1006749/medapp10/question6.java
+++ app/src/main/java/com/example/at1006749/medapp10/question6.java
@@ -1,7 +1,14 @@
 package com.example.at1006749.medapp10;
 
+import android.os.Bundle;
+
 /**
  * Created by 1006749 on 1/21/2016.
  */
 public class question6 {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question_6);
+    }
 }
