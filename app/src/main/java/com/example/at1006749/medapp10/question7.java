c3569b274604aad339841185f3e68ee8edfdc5fc
Pez18000 <ratliffbox@gmail.com>
2016-01-28 10:37:46 -0500

    Added Counter
    This is pretty simple code and I am fairly certain it will work, though
    I have not yet had the chance to test it. Currently the counter only
    increases on question 7, but it is ready to be implemented on every
    question.

--- app/src/main/java/com/example/at1006749/medapp10/question7.java
+++ app/src/main/java/com/example/at1006749/medapp10/question7.java
@@ -19,15 +19,34 @@ import android.widget.Button;
 public class question7 extends Activity {
 
     private Button Next;
+    private Button NextWrong1;
+    private Button NextWrong2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_7);
 
+        NextWrong1 = (Button)findViewById(R.id.button9);
+        NextWrong2 = (Button)findViewById(R.id.button10);
         Next = (Button)findViewById(R.id.button11);
 
         Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.counter++;
+                startActivity(new Intent(question7.this, results.class));
+            }
+        });
+
+        NextWrong1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(question7.this, results.class));
+            }
+        });
+
+        NextWrong2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(question7.this, results.class));
